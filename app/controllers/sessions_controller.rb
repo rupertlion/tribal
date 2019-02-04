f906d9ba5e032d6eb91af5aed55ca73997b792b2
johannaanden <johannaanden@hotmail.com>
2019-02-04 10:35:46 +0100

    Removes index, new, show and get_session actions, removes notice unused notice message and cleans up comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,16 @@
 class SessionsController < ApplicationController
 
-    # before_action :get_session, only: [:show, :index, :create]
-
-    def index
-        @sessions = Session.all
-    end
-
-    def new
-        @session = Session.new
-        @price_tables = PriceTable.all
-    end
-
     def create
         @session = Session.new(session_params)
         @session.save
         if @session.persisted?
-            redirect_to root_path, notice: "Session successfully created"
+            redirect_to root_path
         else
             render json: {errors: 'Every field needs to be filled in!'}
         end
     end
 
-    def show
-        @session = Session.find(params[:id])
-    end
-
     private
-
-    def get_session
-        @session = Session.find(params[:id])
-    end
 
     def session_params
         params.require(:session).permit(:title, :start_date, :price_table_id)
