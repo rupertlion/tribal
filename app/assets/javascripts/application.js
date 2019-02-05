7fd6139eeeb4b641fbd01982313338141fcf9544
hanna_nyman <hannanyman.sk@gmail.com>
2019-02-05 16:26:27 +0100

    Adds videocall controller Adds view for videocall Writes first JavaScript code for the videocall

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,58 @@
 //= require turbolinks
 //= require_tree .
 
+// videocall
+
+const handleFail = () => {
+	console.log("Error: ", err);
+}
+
+let remoteContainer = document.getElementById("#remote-container");
+let canvasContainer = document.getElementById("#canvas-container");
+
+const addVideoStream = (streamId) => {
+	let streamDiv = document.createElement("div");
+	streamDiv.id = streamId;
+	streamDiv.style.transform = "rotateY(180deg)";
+	remoteContainer.appendChild(streamDiv);
+}
+
+const removeVideoStream = () => {
+	let stream = evt.stream;
+	stream.stop();
+	let remDiv = document.getElementById(stream.getId());
+	remDiv.parentNode.removeChild(remDiv);
+	console.log("Remote stream is removed" + stream.getId());
+}
+
+const addCanvas = (streamId) => {
+	let video = document.getElementById(`video${streamId}`);
+	let canvas = document.createElement("canvas");
+	canvasContainer.appendChild(canvas);
+	let ctx = canvas.getContext("2d");
+
+	video.addEventListener("loadedmetadata", event => {
+		canvas.width = video.videoWidth;
+		canvas.height = video.videoHeight;
+	});
+
+	video.addEventListener('play', event => {
+		let $this = this;
+		const loop = () => {
+			if (!$this.paused && !$this.ended) {
+				if ($this.width !== canvas.width) {
+					canvas.width = video.videoWidth;
+					canvas.height = video.videoHeight;
+				}
+				ctx.drawImage($this, 0, 0);
+				setTimeout(loop, 1000 / 30);
+			}
+		}
+	};
+}
+
+// stripe
+
 const stripeTokenHandler = (token) => {
 	let stripeForm = document.getElementById('transaction_form');
 	const hiddenInput = document.createElement('input');
---
+++ app/controllers/videocall_controller.rb
@@ -0,0 +1,7 @@
+class VideocallController < ApplicationController
+
+    def index
+
+    end
+    
+end
