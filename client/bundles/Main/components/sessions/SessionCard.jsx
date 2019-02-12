faabd77e4b53e97191efc042753ecc94160bcc9a
rupertlion <rupertlion@gmail.com>
2019-02-12 14:13:52 +0100

    Adds coach name to session card

--- client/bundles/Main/components/sessions/SessionCard.jsx
+++ client/bundles/Main/components/sessions/SessionCard.jsx
@@ -7,6 +7,7 @@ export default function SessionCard(props) {
 				<div className='session'>
 					<div id={props.session.id} className='session'>
 						<h1 className="session_name">{props.session.title}</h1>
+						<p>{"Coach name:" + props.session.coach_name}</p>
 						<p>{new Date(props.session.start_date).toLocaleString('en-GB', { h12: false })}</p>
 					</div>
 					<div style={props.buttonName ? {} : { display: "none" }} >
