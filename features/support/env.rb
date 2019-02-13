8d8aeb00c3d69304f3b91f0f0b7d344dba268aea
Thomas <thomas@craftacademy.se>
2019-02-13 06:44:50 +0100

    some code cleaning in cucumber config file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,50 +1,50 @@
+# frozen_string_literal: true
+
 require 'cucumber/rails'
 require 'coveralls'
 Coveralls.wear_merged!('rails')
 
 ActionController::Base.allow_rescue = false
 
-Before do
-  OmniAuth.config.test_mode = true
-  OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(OmniAuthFixtures.facebook_mock)
-end
-
 begin
   DatabaseCleaner.strategy = :transaction
+  Cucumber::Rails::Database.javascript_strategy = :truncation
 rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+  raise 'You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it.'
 end
 
 Chromedriver.set_version '2.42'
 
-chrome_options = %w[no-sandbox disable-popup-blocking disable-infobars ]
-
-chrome_options << 'headless'
+chrome_options = %w[no-sandbox disable-popup-blocking disable-infobars]
 
 # Open dev tools in browser (if NOT headless) for debugging code
+chrome_options << 'headless'
 # chrome_options << 'auto-open-devtools-for-tabs'
 
 Capybara.register_driver :selenium do |app|
-	options = Selenium::WebDriver::Chrome::Options.new(
-		args: chrome_options
-)
-Capybara::Selenium::Driver.new(
-		app,
-		browser: :chrome,
-		options: options
-)
-
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: chrome_options
+  )
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: options
+  )
 end
 
-Cucumber::Rails::Database.javascript_strategy = :truncation
-
 World(FactoryBot::Syntax::Methods)
+
 FactoryBot::SyntaxRunner.class_eval do
   include ActionDispatch::TestProcess
 end
 
-Delayed::Worker.delay_jobs do | job |
+Delayed::Worker.delay_jobs do |job|
   job.run_at && job.run_at > Time.now
+end
+
+Before do
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(OmniAuthFixtures.facebook_mock)
 end
 
 Before '@stripe' do
@@ -55,4 +55,3 @@ end
 After '@stripe' do
   StripeMock.stop
 end
-
